e38ed4fd4135d2b76e21cbfb29068325c71bc03a
Sebastian Vizcaino <elvizcacho@gmail.com>
2016-04-15 01:01:02 -0500

    handle exception for Forex api

--- libs/forex.js
+++ libs/forex.js
@@ -4,6 +4,8 @@ var request = require('request');
 module.exports.getPrice = function(base, codestring, cb) {
 	// I assume, I have already a security token
 	request.get('https://api-fxtrade.oanda.com/v1/prices?instruments=' + codestring + '_' + base, function(err, response, body) {
+		if (response.statusCode !== 200)
+			err = JSON.parse(body);
 		cb(err, body);
 	});
 };
